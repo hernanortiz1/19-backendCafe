3d1ebe37fade7844c31cf6efb9bce1c5a1c9af68
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-08-13 20:19:07 -0300

    llegan datos de solicitud

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -8,6 +8,17 @@ export const leerProductos = (req, res)=>{
 
 }
 
-export const crearProductos = (req, res) =>{
+export const crearProductos = async (req, res) =>{
+    try {
+        //recibit obj que tengo que agregagr a BD
+        console.log(req.body)
+    //validar datos del obj
+
+    //guardar obj en BD
+
+    //enviar respuesta
+    } catch (error) {
+        console.error(error);
+    }
     
 }
