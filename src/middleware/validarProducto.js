e7dfc9c610b588a4052c95db1ad30ce429e49c91
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-09-01 21:30:42 -0300

    verificamos que el nombre no este duplicado

--- src/middleware/validarProducto.js
+++ src/middleware/validarProducto.js
@@ -1,12 +1,24 @@
 import { body } from "express-validator";
 import resultadoValidacion from "./resultadoValidacion.js";
+import Producto from "../models/producto.js";
 //cada paso que quiero validar del body, luego se ejecuta validacion para contestar los mensj de error
 const validacionProducto = [
   body("nombreProducto")
     .notEmpty()
     .withMessage("El nombre del producto es obligatorio")
     .isLength({ min: 2, max: 100 })
-    .withMessage("El nombre del producto debe tener entre 2 y 100 caracteres"),
+    .withMessage("El nombre del producto debe tener entre 2 y 100 caracteres")
+    .custom(async (valor, { req }) => {
+      // find() devuelve coleccion de productos, find({agrego por ej una categoria})
+      const productoExistente = await Producto.findOne({
+        nombreProducto: valor
+      });
+      if (!productoExistente) return true
+      //verificar si es PUT
+      if(req.params?.id && productoExistente._id.toString() === req.params.id) return true
+
+      throw new Error("ya existe un producto con ese nombre");
+    }),
   body("precio")
     .notEmpty()
     .withMessage("El precio es un dato obligatorio")
@@ -30,7 +42,7 @@ const validacionProducto = [
   body("categoria")
     .notEmpty()
     .withMessage("La categoria es un dato obligarorio")
-    .isIn("Infusiones", "Batidos", "Dulce", "Salado")
+    .isIn(["Infusiones", "Batidos", "Dulce", "Salado"])
     .withMessage("La categoria debe ser: Infusiones, Batidos, Dulce, Salado"),
   body("descripcion_breve")
     .notEmpty()
