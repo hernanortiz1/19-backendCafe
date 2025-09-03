d6067fa5b26a4c635a3c85c30ff523f5e4332a63
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-09-03 19:57:25 -0300

    agregamos modelo de usuario rutas y controlador

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,9 +1,11 @@
 import productosRoutes from "./productos.routes.js";
 import { Router } from "express";
-
+import usuariosRoutes from "./usuarios.routes.js"
 
 const router = Router()
 
 router.use("/productos", productosRoutes)
+
+router.use("/usuarios", usuariosRoutes)
 
 export default router
