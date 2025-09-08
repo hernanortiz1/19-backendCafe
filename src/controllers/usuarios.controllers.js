14febb96e2a99196cdd8d8e3416163034beba2b6
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-09-08 20:16:29 -0300

    agredamos logica para login usuario

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -1,4 +1,5 @@
 import Usuario from "../models/usuario.js";
+import bcrypt from "bcrypt";
 
 export const leerUsuarios = async (req, res) => {
   try {
@@ -12,7 +13,20 @@ export const leerUsuarios = async (req, res) => {
 
 export const crearUsuario = async (req, res) => {
   try {
-    const nuevoUsuario = new Usuario(req.body);
+    const { nombreUsuario, email, password } = req.body;
+
+    //hashear el password
+    //numero de saltos
+    const saltos = bcrypt.genSaltSync(10);
+    console.log(saltos);
+    //le envio la contraseña y saltos, transforma passs a un valor seguro
+    const passwordHash = bcrypt.hashSync(password, saltos);
+    console.log(passwordHash);
+    const nuevoUsuario = new Usuario({
+      nombreUsuario,
+      email,
+      password: passwordHash,
+    });
     await nuevoUsuario.save();
     //4- enviar respuesta
     res.status(201).json({
@@ -21,5 +35,34 @@ export const crearUsuario = async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ mensaje: "Error al crear el usuario" });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    //verificar si el email existe, si no existe enviar msj error
+    const { email, password } = req.body;
+
+    //verifica si existe en bd
+    const usuarioExistente = await Usuario.findOne({ email });
+    if (!usuarioExistente) {
+      return res.status(404).json({ mensaje: "No se encontro el usuario" });
+    }
+    //si existe email, verificar password, si no es el mismo error
+    const passwordVerificado = bcrypt.compareSync(
+      password,
+      usuarioExistente.password
+    );
+    if (!passwordVerificado) {
+      return res.status(401).json({ mensaje: "Credenciales incorrectas" });
+    }
+
+    //generar token
+
+    //enviar respuesta a front
+    res.status(200).json({mensaje: "login exitoso", nombreUsuario: usuarioExistente.nombreUsuario})
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensaje: "Error al loguear el usuario" });
   }
 };
